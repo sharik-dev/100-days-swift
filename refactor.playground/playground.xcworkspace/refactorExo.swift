974ba8c71d48725058f0cef0dcad0ae6835db4ae
cdfleader <sharikmohamed8@gmail.com>
2025-12-16 23:23:27 +0100

    refactor exo

---
+++ refactor.playground/playground.xcworkspace/refactorExo.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func doStuff(a: Int, b: Int, c: Bool) -> Int {
+    var result = 0
+
+    if c == true {
+        if a > b {
+            result = a - b
+        } else {
+            result = b - a
+        }
+    } else {
+        if a > b {
+            result = a + b
+        } else {
+            result = b + a
+        }
+    }
+
+    print("The result is \(result)")
+    return result
+}
+
+let x = doStuff(a: 10, b: 3, c: true)
+let y = doStuff(a: 4, b: 9, c: false)
